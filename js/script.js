eea9cad47515574bf8154e231c1a5027d8cb9b4b
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-15 16:45:53 +0200

    added script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
- 
+let spesa = ['pasta','carne','pesce','biscotti'];
+
+let contatore = 0;
+
+const listContainerEl = document.getElementById("list-container");
+
+while (contatore !== spesa.length ) {
+    let item = document.createElement("li");
+    item.innerHTML = spesa[contatore];
+    listContainerEl.appendChild(item);
+    contatore ++;
+}
